7c814139b0702789cd8db65b3aa1aa8862761633
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-08-08 14:12:57 -0400

    Board connects, disconnects, and reconnects automatically.

--- Python/GarnetControls/HAL.py
+++ Python/GarnetControls/HAL.py
@@ -1,7 +1,8 @@
 import serial
 import serial.tools.list_ports as lp
 from crccheck.crc import Crc8Maxim
-import wx
+import time
+#import wx
 import threading
 import numpy
 
@@ -12,25 +13,14 @@ class HAL:
     ANALOG_INPUTS = 16
     SWITCH_INPUTS = 16
 
-    def __init__(self, port_name='auto', speed=115200, timeout=1, PID=24577, VID=1027):
+    def __init__(self, port_name='auto', speed=115200, timeout=1, PID=24577, VID=1027, debug=False):
 
 
+        self.port_name = port_name
+        self.speed = speed
         self.timeout = timeout
-
-        # If the port name is auto, find the control board by searching throughthe PID and VID
-        if port_name == 'auto':
-            port_names = [port.device for port in lp.comports() if port.pid == PID and port.vid == VID]
-            if not any(port_names):
-                raise serial.SerialException('No valid COM ports found for the control board. Is it connected?')
-            elif len(port_names) > 1:
-                # TODO: Handle multiple port_names
-                port_name = port_names[0]
-            else:
-                port_name = port_names[0]
-
-        self.port = serial.Serial(port=port_name, baudrate=speed, timeout=self.timeout)
-
-
+        self.PID = PID
+        self.VID = VID
 
         # Set up default variable
         self.led_out = None
@@ -39,16 +29,156 @@ class HAL:
         self.switch_in = None
         self.reset_values()
 
+        self.port = None
+
+        self.debug = debug
+
+        self.run_thread = False
+
+    def usb_connect(self):
+        port_name = self.find_com_port()
+        self.port = serial.Serial(port=port_name, baudrate=self.speed, timeout=self.timeout)
+
+    def find_com_port(self):
+        # If the port name is auto, find the control board by searching through the PID and VID
+        port_names = [port.device for port in lp.comports() if port.pid == self.PID and port.vid == self.VID]
+
+        if self.port_name == 'auto':
+            if not any(port_names):
+                raise serial.SerialException('No valid COM ports found for the control board. Is it connected?')
+                time.sleep(1)
+            elif len(port_names) > 1:
+                # TODO: Handle multiple port_names
+                port_name = port_names[0]
+            else:
+                port_name = port_names[0]
+        else:
+            if self.port_name in port_names:
+                port_name = self.port_name
+            else:
+                raise serial.SerialException('Invalid COM port selected.')
+
+        return port_name
+
+
+    def usb_disconnect(self):
+        if self.usb_connected():
+            self.port.close()
+
+    def usb_reconnect(self):
+        self.usb_disconnect()
+        time.sleep(1)
+        self.usb_connect()
+
     def reset_values(self):
         self.led_out = [False] * self.LED_OUTPUTS
         self.pwm_out = [0] * self.PWM_OUTPUTS
         self.analog_in = [0.0] * self.ANALOG_INPUTS
         self.switch_in = [False] * self.SWITCH_INPUTS
 
+    def reset_board(self):
+
+        self.port.flushInput()
+
+        # Reset
+        self.port.dtr = 1;
+        time.sleep(50e-3)
+        self.port.dtr = 0;
+
+        # Read welcome message
+        welcome_msg = self.port.readline().decode('utf-8')
+
+        if welcome_msg != 'FRC Control Board\r\n':
+            raise ConnectionError('FRC control board did not send welcome message after reset.')
+
+    def usb_connected(self):
+        if self.port is None:
+            return False
+        else:
+            return self.port.isOpen()
+
+    def start(self):
+        self.run_thread = True
+        self.thread = threading.Thread(target=self.run, daemon=True)
+        self.thread.start()
+
+    def stop(self):
+        if self.run_thread is True:
+            self.run_thread = False
+            self.thread.join()
+
 
     def run(self):
-        while self.port.is_open():
-            pass
+        run_state_machine = True
+        STATE_INIT = 'Init'
+        STATE_CHECK_CONNECTION = 'Check Connection'
+        STATE_RESET = 'Reset'
+        STATE_RUN = 'Run'
+        STATE_RECONNECT = 'Reconnect'
+        STATE_STOP = 'Stop'
+
+        last_state = STATE_INIT
+        state = STATE_INIT
+
+
+        while run_state_machine:
+            # Stop case
+            if self.run_thread is False:
+                state = STATE_STOP
+
+            # Debug
+            if self.debug is True and state is not last_state:
+                print('HAL mode switch: %s -> %s' % (last_state, state))
+                last_state = state
+
+            # State machine
+            try:
+                if state is STATE_INIT:
+                    if self.debug:
+                        print('HAL Started.')
+                    state = STATE_CHECK_CONNECTION
+
+                if state is STATE_CHECK_CONNECTION:
+                    if self.usb_connected():
+                        state = STATE_RESET
+                    else:
+                        state = STATE_RECONNECT
+                elif state is STATE_RESET:
+                    self.reset_board()
+                    state = STATE_RUN
+                elif state is STATE_RUN:
+                    self.update()
+                    state = STATE_RUN
+                elif state is STATE_RECONNECT:
+                    self.usb_reconnect()
+                    state = STATE_CHECK_CONNECTION
+                elif state is STATE_STOP:
+                    run_state_machine = False
+                    if self.debug:
+                        print('HAL Stopped.')
+                    state = STATE_STOP
+            except serial.SerialException:
+                state = STATE_RECONNECT
+            except serial.SerialTimeoutException:
+                state = STATE_RESET
+            except KeyboardInterrupt:
+                state = STATE_STOP
+            except Exception as e:
+                if self.debug:
+                    print('Unhandled exception:', str(e))
+
+    def update(self):
+        data_out = self.pack_data(self.led_out, self.pwm_out)
+        # print ('OUT:', data_out)
+        self.port.write(str.encode(data_out+'\r\n'))
+        self.port.flushOutput()
+        try:
+            data_in = self.port.readline().decode('utf-8')
+            # print ('IN:', data_in)
+            self.switch_in, self.analog_in = self.unpack_data(data_in)
+
+        except Exception as e:
+            print(str(e))
 
     def pack_data(self, led_array, pwm_array):
 
@@ -80,13 +210,13 @@ class HAL:
 
         return data_crc_out
 
-
-    @staticmethod
     def unpack_data(self, data_string):
+        # 'SW:0;ANA:4,4,6,4,4,4,5,4,4,4,4,5,5,4,4,6;CRC:162;\r\n'
         analog_array = [0.0] * self.ANALOG_INPUTS
+        switch_array = [False] * self.SWITCH_INPUTS
 
 
-        return (analog_array, switch_array)
+        return switch_array, analog_array
 
 
 
