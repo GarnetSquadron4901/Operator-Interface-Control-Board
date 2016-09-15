edf3365cbc24c558441d881ec73f75a8041a5d13
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-09-14 22:04:57 -0400

    Finished the simulator.

--- Python/ControlBoardApp/hal/Simulator.py
+++ Python/ControlBoardApp/hal/Simulator.py
@@ -23,6 +23,8 @@ class HardwareAbstractionLayer(ControlBoardBase):
 
         self.sim = None
 
+
+
     def set_sim_connection(self, sim):
         self.sim = sim
 
@@ -40,17 +42,22 @@ class HardwareAbstractionLayer(ControlBoardBase):
 
     def update(self):
 
+        if not self.is_connected():
+
+            raise ConnectionFailed('The simulator connection status is False.')
+
         if self.sim is not None:
-            pwms = self.getPwmValues()
-            leds = self.getLedValues()
+            self.led_out[0] = True
+            self.sim.set_pwms(self.getPwmValues())
+            self.sim.set_leds(self.getLedValues())
 
             # Update screen
 
-            self.sim.update_indicators(pwms=pwms, leds=leds, )
+            self.sim.update_indicators()
             time.sleep(20e-3)
 
-            self.putAnalogvalues(analogs)
-            self.putSwitchvalues(switches)
+            self.putAnalogvalues(self.sim.get_analogs())
+            self.putSwitchvalues(self.sim.get_switches())
         else:
             raise ConnectionFailed('The simulator is not running yet.')
 
@@ -71,6 +78,8 @@ class HardwareAbstractionLayer(ControlBoardBase):
 class SimulatorFrame(wx.Frame):
 
     DEFAULT_STATUS = '-                           '
+    LED_ON = (0, 160, 0)
+    LED_OFF = (160, 0, 0)
 
     def __init__(self, parent, hal):
         self.parent = parent
@@ -106,11 +115,10 @@ class SimulatorFrame(wx.Frame):
         self.switch_status = self.tree.AppendItem(self.tree.GetRootItem(), 'Switch Inputs')
 
         self.LED_Status = []
+
         for array_index in range(self.hal.LED_OUTPUTS):
             label = self.tree.AppendItem(self.led_status, 'LED ' + str(array_index))
-            item = wx.CheckBox(self)
-            item.Disable()
-            item.SetValue(False)
+            item = wx.StaticText(self, label=self.DEFAULT_STATUS)
             self.LED_Status.append(item)
             self.tree.SetItemWindow(label, item, 1)
 
@@ -122,7 +130,6 @@ class SimulatorFrame(wx.Frame):
             self.tree.SetItemWindow(label, item, 1)
 
         self.ANA_Status = []
-        self.analogs_out = []
         for array_index in range(self.hal.ANALOG_INPUTS):
             label = self.tree.AppendItem(self.analog_status, 'ANA ' + str(array_index))
             item = wx.Slider(self)
@@ -132,12 +139,16 @@ class SimulatorFrame(wx.Frame):
             self.tree.SetItemWindow(label, item, 1)
 
         self.SW_Status = []
-        self.switches_out = []
         for array_index in range(self.hal.SWITCH_INPUTS):
             label = self.tree.AppendItem(self.switch_status, 'SW ' + str(array_index))
             item = wx.CheckBox(self)
             self.SW_Status.append(item)
             self.tree.SetItemWindow(label, item, 1)
+
+        self.analog_in = [0] * self.hal.ANALOG_INPUTS
+        self.switch_in = [False] * self.hal.SWITCH_INPUTS
+        self.pwm_in = [0] * self.hal.PWM_OUTPUTS
+        self.led_in = [False] * self.hal.LED_OUTPUTS
 
 
         self.tree.CalculateAndSetHeaderHeight()
@@ -153,24 +164,40 @@ class SimulatorFrame(wx.Frame):
         self.SetSizer(self.v_sizer)
         self.Layout()
 
+        self.Refresh()
+
+    def set_pwms(self, pwms):
+        self.pwm_in = pwms
+
+    def set_leds(self, leds):
+        self.led_in = leds
+
+    def get_analogs(self):
+        return self.analogs_out
+
+    def get_switches(self):
+        return self.switches_out
+
     def is_connected(self):
         return self.connection_status.GetValue()
 
     def put_tree_data(self, gui_element, status):
         if gui_element.GetLabelText() != str(status):
             gui_element.SetLabelText(str(status))
+            return True
+        else:
+            return False
 
-    def _update_indicators(self, pwms, leds):
-
+    def _update_indicators(self):
         for channel in range(self.hal.LED_OUTPUTS):
-            self.put_tree_data(self.LED_Status[channel], leds[channel])
+            if self.put_tree_data(self.LED_Status[channel], ('On' if self.led_in[channel] else 'Off')):
+                self.LED_Status[channel].SetForegroundColour(self.LED_ON if self.led_in[channel] else self.LED_OFF)
 
         for channel in range(self.hal.PWM_OUTPUTS):
-            self.put_tree_data(self.PWM_Status[channel], pwms[channel])
+            self.put_tree_data(self.PWM_Status[channel], self.pwm_in[channel])
 
         self.analogs_out = [analog.GetValue() for analog in self.ANA_Status]
         self.switches_out = [switch.GetValue() for switch in self.SW_Status]
 
-
-    def update_indicators(self, pwms, leds):
-        return wx.CallAfter(self._update_indicators, pwms, leds)
+    def update_indicators(self):
+        return wx.CallAfter(self._update_indicators)
