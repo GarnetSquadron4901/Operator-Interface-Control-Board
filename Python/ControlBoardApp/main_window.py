251c6386208415c93ec7ccf269e239bea88247bb
Ryan Nazaretian <ryannazaretian@gmail.com>
2016-09-15 20:08:46 -0400

    Test mode is working, albeit, the PWM sliders are acting weird with the background. About window made.

--- Python/ControlBoardApp/main_window.py
+++ Python/ControlBoardApp/main_window.py
@@ -1,8 +1,11 @@
-import wx
+import wx, wx.html
 import wx.lib.agw.hypertreelist as HTL
 from wx.adv import TaskBarIcon as TBI
 import ctypes
 import os
+import sys
+import ControlBoardApp._version as ntcbaver
+import networktables.version as ntver
 
 # Main application icon
 MAIN_ICON = os.path.abspath(os.path.join(os.path.split(__file__)[0], 'ControlBoard.ico'))
@@ -54,6 +57,41 @@ class TaskBarIcon(TBI):
         icon.CopyFromBitmap(wx.Bitmap(path, wx.BITMAP_TYPE_ANY))
         self.SetIcon(icon, TRAY_TOOLTIP)
 
+class HtmlWindow(wx.html.HtmlWindow):
+    def __init__(self, parent, id, size=(600,400)):
+        wx.html.HtmlWindow.__init__(self,parent, id, size=size)
+        if "gtk2" in wx.PlatformInfo:
+            self.SetStandardFonts()
+        self.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.OnLinkClicked)
+
+    def OnLinkClicked(self, event):
+        os.startfile(event.GetLinkInfo().GetHref())
+
+aboutText = """<p>FRC Control Board Application: %(cbaver)s</p>
+<p>pynetworktables: %(ntver)s</p>
+<p>wxPython: %(wxpy)s</p>
+<p>Python: %(python)s</p>
+<p>See <a href="http://github.com/GarnetSquardon4901/Operator-Interface-Control-Board">FRC Control Board Documentation</a></p>
+"""
+class AboutBox(wx.Dialog):
+    def __init__(self):
+        wx.Dialog.__init__(self, None, -1, "About FRC Control Board",
+            style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|
+                wx.TAB_TRAVERSAL)
+        hwin = HtmlWindow(self, -1, size=(400,200))
+        vers = {}
+        vers["python"] = sys.version.split()[0]
+        vers["wxpy"] = wx.VERSION_STRING
+        vers["cbaver"] = str(ntcbaver.__version__)
+        vers["ntver"] = str(ntver.__version__)
+        hwin.SetPage(aboutText % vers)
+        btn = hwin.FindWindowById(wx.ID_OK)
+        irep = hwin.GetInternalRepresentation()
+        hwin.SetSize((irep.GetWidth()+25, irep.GetHeight()+10))
+        self.SetClientSize(hwin.GetSize())
+        self.CentreOnParent(wx.BOTH)
+        self.SetFocus()
+
 class MainWindow(wx.Frame):
 
     DEFAULT_STATUS = '-                           '
@@ -94,13 +132,15 @@ class MainWindow(wx.Frame):
         self.menu_settings_testmode = self.menu_settings.AppendCheckItem(wx.ID_ANY, 'Test Mode', 'Enable/Disable Test Mode')
         self.menu_settings_setaddy = self.menu_settings.Append(wx.ID_ANY, 'Set NT Address', 'Set the robot\'s address to access the Network Table server')
         self.Bind(wx.EVT_MENU, self.OnTestModeChanged, self.menu_settings_testmode)
-        self.Bind(wx.EVT_MENU, self.OnSetRobotAddress, self.menu_settings_setaddy)
+        self.Bind(wx.EVT_MENU, self.OnSetNtAddress, self.menu_settings_setaddy)
 
 
         # Help menu
         self.menu_help = wx.Menu()
         self.menu_help_help = self.menu_help.Append(wx.ID_ANY, 'Help', 'Shows the documentation for the control board and application')
         self.menu_help_about = self.menu_help.Append(wx.ID_ANY, 'About', 'About this application')
+        self.Bind(wx.EVT_MENU, self.OnHelp, self.menu_help_help)
+        self.Bind(wx.EVT_MENU, self.OnAbout, self.menu_help_about)
 
 
         # Create menu bar
@@ -120,6 +160,7 @@ class MainWindow(wx.Frame):
                                       )
         self.tree.AddColumn('Name')
         self.tree.AddColumn('Status')
+        self.tree.AddColumn('Test Mode')
         self.tree.AddRoot('Root')
         self.tree.SetMainColumn(0)
 
@@ -137,18 +178,28 @@ class MainWindow(wx.Frame):
         self.switch_status = self.tree.AppendItem(self.tree.GetRootItem(), 'Switch Inputs')
 
         self.LED_Status = []
+        self.LED_Test = []
         for array_index in range(self.hal.LED_OUTPUTS):
             label = self.tree.AppendItem(self.led_status, 'LED ' + str(array_index))
             item = wx.StaticText(self, label=self.DEFAULT_STATUS)
+            test = wx.CheckBox(self)
             self.LED_Status.append(item)
+            self.LED_Test.append(test)
             self.tree.SetItemWindow(label, item, 1)
+            self.tree.SetItemWindow(label, test, 2)
 
         self.PWM_Status = []
+        self.PWM_Test = []
         for array_index in range(self.hal.PWM_OUTPUTS):
             label = self.tree.AppendItem(self.pwm_status, 'PWM ' + str(array_index))
             item = wx.StaticText(self, label=self.DEFAULT_STATUS)
+            test = wx.Slider(self)
+            test.SetMin(0)
+            test.SetMax(255)
+            self.PWM_Test.append(test)
             self.PWM_Status.append(item)
             self.tree.SetItemWindow(label, item, 1)
+            self.tree.SetItemWindow(label, test, 2)
 
         self.ANA_Status = []
         for array_index in range(self.hal.ANALOG_INPUTS):
@@ -176,19 +227,53 @@ class MainWindow(wx.Frame):
         self.v_sizer.SetMinSize(self.tree.GetBestSize())
 
         self.Bind(wx.EVT_CLOSE, self.exit_app)
+        self.Bind(wx.EVT_TREE_ITEM_EXPANDED, self.update_test_elements)
 
+        self.SetMenuBar(self.menu)
 
         self.SetSizer(self.v_sizer)
-        self.SetMenuBar(self.menu)
 
         self.SetAutoLayout(True)
         self.Layout()
 
-    def OnTestModeChanged(self, event):
-        print('Test mode changed!')
+        self.OnTestModeChanged()
 
-    def OnSetRobotAddress(self, event):
-        pass
+
+    def OnTestModeChanged(self, _=None):
+        print ('Test mode changed to', ('On' if self.isTestModeEnabled() else 'Off'))
+
+        self.update_test_elements()
+        self.tree.GetColumn(2).SetShown(self.isTestModeEnabled())
+        self.tree.Refresh(True)
+        self.tree.Update()
+        self.Refresh(True)
+
+    def update_test_elements(self, _=None):
+        test_mode_enabled = self.isTestModeEnabled()
+
+        if self.led_status.IsExpanded():
+            for led in self.LED_Test:
+                led.Show(test_mode_enabled)
+            led.GetParent().Refresh()
+
+        if self.pwm_status.IsExpanded():
+            for pwm in self.PWM_Test:
+                pwm.Show(test_mode_enabled)
+            pwm.GetParent().Refresh()
+
+
+
+
+    def OnSetNtAddress(self, _=None):
+        print('Set NT address pressed')
+
+    def OnAbout(self, _=None):
+        dlg = AboutBox()
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnHelp(self, _=None):
+        print('Help button pressed')
 
     def hide_window(self):
         self.Hide()
@@ -203,8 +288,18 @@ class MainWindow(wx.Frame):
         self.Hide()
         self.Destroy()
 
+    def isTestModeEnabled(self):
+        return self.menu_settings_testmode.IsChecked()
+
+    def updateHalWithTestValues(self, _=None):
+        self.hal.putLedValues([led.IsChecked() for led in self.LED_Test])
+        self.hal.putPwmValues([pwm.GetValue() for pwm in self.PWM_Test])
+
     def event_responder(self, event):
-        self.nt.update()
+        if self.isTestModeEnabled():
+            wx.CallAfter(self.updateHalWithTestValues, event)
+        else:
+            self.nt.update()
         wx.CallAfter(self.update_indicators, event)
 
     def get_hal_status(self, is_running, state, update_rate):
@@ -239,17 +334,19 @@ class MainWindow(wx.Frame):
 
             if data['IsRunning']:
 
+                for channel in range(self.hal.ANALOG_INPUTS):
+                    self.update_tree_status(self.ANA_Status[channel], data['ANAs'][channel])
+
+                for channel in range(self.hal.SWITCH_INPUTS):
+                    self.update_tree_status(self.SW_Status[channel], 'Closed' if data['SWs'][channel] else 'Open')
+
                 for channel in range(self.hal.LED_OUTPUTS):
                     self.update_tree_status(self.LED_Status[channel], 'On' if data['LEDs'][channel] else 'Off')
 
                 for channel in range(self.hal.PWM_OUTPUTS):
                     self.update_tree_status(self.PWM_Status[channel], data['PWMs'][channel])
 
-                for channel in range(self.hal.ANALOG_INPUTS):
-                    self.update_tree_status(self.ANA_Status[channel], data['ANAs'][channel])
 
-                for channel in range(self.hal.SWITCH_INPUTS):
-                    self.update_tree_status(self.SW_Status[channel], 'Closed' if data['SWs'][channel] else 'Open')
             else:
 
                 for channel in range(self.hal.LED_OUTPUTS):
